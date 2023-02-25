c27063b3e4a73f0efd6db2c1b0ef0c39500b31a4
Yuval Dolev <yuvaldolev2@gmail.com>
2023-02-25 13:02:18 +0200

    feat(renderer): work on Renderer

--- src/invaders.rs
+++ src/invaders.rs
@@ -1,3 +1,5 @@
+use glam::Vec2;
+
 use crate::error;
 use crate::renderer::Renderer;
 use crate::window::Window;
@@ -26,13 +28,25 @@ impl Invaders {
             // Clear the frame events.
             frame_events.clear();
 
+            // Process the input.
             // Update the window events.
             self.window.update(&mut frame_events);
 
             // Simulate the game.
+            // TODO: This should be moved to dedicated Game code:
+            // ```
+            // game.simulate(&frame_events);
+            // ```
             for event in frame_events.iter() {
                 log::debug!("Event: {:?}", event);
             }
+
+            // Render the game.
+            // TODO: This should be moved to dedicated Game code
+            // ```
+            // game.render(&self.renderer);
+            // ```
+            self.renderer.draw_quad(Vec2::new();
 
             // Swap the window buffers.
             self.window.swap_buffers();
